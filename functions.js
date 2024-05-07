65046d44e1ea7ea6aa73518ae27e48b87c2e2f2b
Argentino010 <159962042+Argentino010@users.noreply.github.com>
2024-05-07 16:31:22 +0200

    Update functions.js
    con ayuda del chat gpt

--- functions.js
+++ functions.js
@@ -4,25 +4,23 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if (cityName === "") return;
-    const API_KEY = "2310c9bcb50eb1c17070bc5b3ac3a57c"
+    const API_KEY = "2310c9bcb50eb1c17070bc5b3ac3a57c";
     const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     fetch(API_URL)
         .then((response) => response.json())
         .then((data) => {
-            if (!data.length)
-                return alert('No coordinates found for ${cityName}');
+            if (!data.length) {
+                return alert(`No coordinates found for ${cityName}`);
+            }
             const { lat, lon, name } = data[0];
             getWeatherDetails(name, lat, lon);
         })
-
         .catch(() => {
-            alert("An error ocurred while fetching the coordinates!");
-
+            alert("An error occurred while fetching the coordinates!");
         });
-
 };
+
